68f8b344bf74b27517fafd5fccda78b02c9862fa
Raqeeb Thassim <Raqeebthassim@gmail.com>
2023-02-27 15:55:53 +0530

    Server code updated

--- server/nodejs/app.js
+++ server/nodejs/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var upmarketRouter = require('./routes/upmarket.router');
@@ -10,8 +9,6 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', upmarketRouter);
 
