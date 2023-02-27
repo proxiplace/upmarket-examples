9a304f9e7e5ccfbd26607fdc65e13af56ed96b49
Codeslip <jason@upmarket.ai>
2023-02-27 11:32:31 -0500

    removed number generator

--- client/typescript/src/components/Home.tsx
+++ client/typescript/src/components/Home.tsx
@@ -8,19 +8,15 @@ function Home() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   /** Use to generate a generate a random client_lead_id */
-  function getRandomInt(max: number) {
-    return Math.floor(Math.random() * max);
-  }
 
   /** Add the following after the user is successfully logged in */
   async function handleLogin() {
     const leadInitializeData: LeadInitialize = {
-      client_lead_id: "< Unique identifier for your lead >", // This is the unique identifier from your system.
+      client_lead_id: "example_lead_id", // This is the unique identifier from your system, use the 
       journey_id: "< Enter Journey Id here! >", // Upmarket Journey Id
       lead_type: "< `account` or `contact` >", // Leads are either an account or contact
       lead_name: "< Enter Lead Name here! >", // String identifying for your lead
     };
-
     await initializeLead(leadInitializeData);
   }
 
@@ -31,7 +27,6 @@ function Home() {
       lead_id: "< Enter Lead Id here! >", // The lead_id returned from intializeLead()
       event_id: eventId, // The event_id representing the event you're creating
     };
-
     await createEvent(eventCreateData);
   }
 
